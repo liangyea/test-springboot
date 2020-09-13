e9855ba0489ff995875c7f448cc56a80ac0fd5d7
liangyea <332770873@qq.com>
2020-09-13 17:01:39 +0800

    first commit

---
+++ src/main/java/com/vincent/testspringboot/TestSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.vincent.testspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringbootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringbootApplication.class, args);
+	}
+
+}
